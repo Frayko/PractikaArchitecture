9bd9b73d4eb411853108920ebff21d535ff2e4c7
Alex <sasha.1.sas@mail.ru>
2021-11-27 16:42:32 +0700

    Добавил сырые данные Feed

---
+++ PractikaArchitecture/PractikaArchitecture/Modules/Screen-2/Models/FeedsCollection.swift
@@ -0,0 +1,24 @@
+//
+//  FeedsCollection.swift
+//  PractikaArchitecture
+//
+//  Created by Александр Фомин on 27.11.2021.
+//
+
+import UIKit
+
+final class FeedsCollection {
+	private lazy var feeds: [FeedModel] = {
+		return getFeeds()
+	}()
+}
+
+extension FeedsCollection {
+	func getFeeds() -> [FeedModel] {
+		feeds
+	}
+}
+
+private extension FeedsCollection {
+
+}
---
+++ PractikaArchitecture/PractikaArchitecture/Raw Data/Feed.swift
@@ -0,0 +1,14 @@
+//
+//  Feeds.swift
+//  PractikaArchitecture
+//
+//  Created by Александр Фомин on 27.11.2021.
+//
+
+struct Feed {
+	let imageName: String
+	let title: String
+	let author: String
+	let description_mini: String
+	let description_max: String
+}
