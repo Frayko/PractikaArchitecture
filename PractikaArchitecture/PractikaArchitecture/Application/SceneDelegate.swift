10057296768ce0e677bb117f252621512c205de3
Alex <sasha.1.sas@mail.ru>
2021-12-03 22:08:56 +0700

    Добавил AppNavigationController и хранилище сырых статей

--- PractikaArchitecture/PractikaArchitecture/Application/SceneDelegate.swift
+++ PractikaArchitecture/PractikaArchitecture/Application/SceneDelegate.swift
@@ -14,7 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let windowScene = (scene as? UIWindowScene) else { return }
 		let window = UIWindow(windowScene: windowScene)
-		
+		let appNavigationController = AppNavigationController()
+		window.rootViewController = appNavigationController
 		self.window = window
 		self.window?.makeKeyAndVisible()
 		
